3a7b9111f2983f9c0533bb930db8c357e2ce32fc
10000081 <10000081@hsbk.nb>
2020-09-08 18:44:39 +0600

    Remove some imports as Component and Autowired

--- src/main/java/org/springframework/MusicPlayer.java
+++ src/main/java/org/springframework/MusicPlayer.java
@@ -1,8 +1,6 @@
 package org.springframework;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
 
 public class MusicPlayer {
 
